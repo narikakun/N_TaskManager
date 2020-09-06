9f811e6af69f95ee59726480b9d38c968ab9d2ce
なりかくん <narikakun@gmail.com>
2020-09-06 19:30:34 +0900

    プロジェクト ファイルを追加します。

---
+++ N_TaskManager/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace N_TaskManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.get_button = new System.Windows.Forms.ToolStripButton();
+            this.name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.filename = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.totaltime = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.workmem = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.name,
+            this.id,
+            this.filename,
+            this.totaltime,
+            this.workmem});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 28);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 21;
+            this.dataGridView1.Size = new System.Drawing.Size(776, 410);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.get_button});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(800, 25);
+            this.toolStrip1.TabIndex = 2;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // get_button
+            // 
+            this.get_button.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.get_button.Image = ((System.Drawing.Image)(resources.GetObject("get_button.Image")));
+            this.get_button.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.get_button.Name = "get_button";
+            this.get_button.Size = new System.Drawing.Size(23, 22);
+            this.get_button.Text = "取得";
+            this.get_button.Click += new System.EventHandler(this.get_button_Click);
+            // 
+            // name
+            // 
+            this.name.HeaderText = "プロセス名";
+            this.name.Name = "name";
+            this.name.ReadOnly = true;
+            // 
+            // id
+            // 
+            this.id.HeaderText = "プロセスID";
+            this.id.Name = "id";
+            this.id.ReadOnly = true;
+            // 
+            // filename
+            // 
+            this.filename.HeaderText = "ファイル名";
+            this.filename.MinimumWidth = 20;
+            this.filename.Name = "filename";
+            this.filename.ReadOnly = true;
+            // 
+            // totaltime
+            // 
+            this.totaltime.HeaderText = "合計プロセッサ時間";
+            this.totaltime.MinimumWidth = 10;
+            this.totaltime.Name = "totaltime";
+            this.totaltime.ReadOnly = true;
+            // 
+            // workmem
+            // 
+            this.workmem.HeaderText = "物理メモリ使用量";
+            this.workmem.MinimumWidth = 10;
+            this.workmem.Name = "workmem";
+            this.workmem.ReadOnly = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton get_button;
+        private System.Windows.Forms.DataGridViewTextBoxColumn name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn filename;
+        private System.Windows.Forms.DataGridViewTextBoxColumn totaltime;
+        private System.Windows.Forms.DataGridViewTextBoxColumn workmem;
+    }
+}
+
---
+++ N_TaskManager/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace N_TaskManager
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void get_button_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process[] ps = System.Diagnostics.Process.GetProcesses();
+
+            foreach (System.Diagnostics.Process p in ps)
+            {
+                try
+                {
+                    dataGridView1.Rows.Add(p.ProcessName, p.Id, p.MainModule.FileName, p.TotalProcessorTime, p.WorkingSet64);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+    }
+}
